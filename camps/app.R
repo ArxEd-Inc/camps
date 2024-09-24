f2b3e6269516430ac577393495bea3731dcba59d
Santiago Casanova <santiago.casanova@yahoo.com>
2024-09-23 22:48:02 -0600

    added button, corrected date

--- camps/app.R
+++ camps/app.R
@@ -1,14 +1,29 @@
 library(shiny)
 
 ui <- fluidPage(
-  includeHTML('www/index.html')
+  HTML('
+  <html>
+<head>
+    <title>ArxEd YETI</title>
+    <link rel="shortcut icon" href="favicon.png" type="image/png">
+    <style> 
+      body{background-color:#27396A}
+    </style>
+</head>
+
+<body>
+<iframe title="ArxEd Yeti" width="1140" height="541.25" src="https://arxed-inc.github.io/camps/" frameborder="0" allowFullScreen="true" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
+</body>
+</html>
+  
+      ')
   
 )
 
 server <- function(input, output, session) {
   # Server logic goes here
   
-  observe(print(input$name))
+  
 }
 
 shinyApp(ui = ui, server = server)
