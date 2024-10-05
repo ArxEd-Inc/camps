251ac33c7b0cbe5cc6fff7f65cdf5048177f0ff9
Santiago Casanova <santiago.casanova@yahoo.com>
2024-10-05 11:14:31 -0600

    Fix youtube target blank

--- camps/app.R
+++ camps/app.R
@@ -6,7 +6,7 @@ ui <- fluidPage(
 <head>
     <title>ArxEd SPARC</title>
     <link rel="shortcut icon" href="favicon.png" type="image/png">
-    <style> 
+    <style>
       body{background-color:#27396A}
     </style>
 </head>
@@ -15,7 +15,7 @@ ui <- fluidPage(
 <iframe title="ArxEd Yeti" width="1140" height="541.25" src="https://arxed-inc.github.io/camps/" frameborder="0" allowFullScreen="true" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
 </body>
 </html>
-  
+
       ')
   
 )
