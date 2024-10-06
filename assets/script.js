6432ffb3d35d253e8f5d685184e30d20c409edbd
Santiago Casanova <santiago.casanova@yahoo.com>
2024-10-06 15:09:46 -0600

    Change hour, fix timer end

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 const year = new Date().getFullYear();
-const fourthOfJuly = new Date(year, 9,9).getTime();
-const fourthOfJulyNextYear = new Date(year , 9, 9).getTime();
+const test = new Date(year, 9,8, 18).getTime();
 const month = new Date().getMonth();
+
+console.log(test);
 
 // countdown
 let timer = setInterval(function() {
@@ -10,14 +11,37 @@ let timer = setInterval(function() {
   const today = new Date().getTime();
 
   // get the difference
-  let diff = fourthOfJuly - today;
+  let diff = test - today;
 
   // math
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  let days;
+  let hours;
+  let minutes;
+  let seconds;
+  
+  if(diff <0 ){
+    days = 0
+  } else{
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  }
 
+  if(diff <0 ){
+    hours = 0
+  } else{
+    hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  }
+
+  if(diff <0 ){
+    minutes = 0
+  } else{
+    minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  }
+
+  if(diff <0 ){
+    seconds = 0
+  } else{
+    seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  }
 
   // display
   document.getElementById("timer").innerHTML =
